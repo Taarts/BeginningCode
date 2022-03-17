d42f55df6da1fbfa21f6101970884a013157a279
Tricia Amheiser <tricia@taarts.net>
2022-03-17 14:27:20 -0400

    first change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace BeginningCode
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Change");
+      string characterName = "Clare";
+      int characterHeight;
+      characterHeight = 6;
+
+      Console.WriteLine("There once was a girl called Clare");
+      Console.WriteLine("She was 6 feet tall");
+      Console.WriteLine("She liked the name Clare");
+      Console.WriteLine("But she didn't like being 6 feet tall");
     }
   }
 }
