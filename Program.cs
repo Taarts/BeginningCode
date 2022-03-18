89083f8edeb66c130a34d1a40759b8676a5795b8
Tricia Amheiser <tricia@taarts.net>
2022-03-18 18:11:02 -0400

    Input from user converto to integer & Combine vale

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ namespace BeginningCode
   {
     static void Main(string[] args)
     {
-    int num = Convert.ToInt32("56");
-     Console.WriteLine(num + 545);
+
+     Console.Write("Enter a number: ");
+      double num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter another number: ");
+      double num2 = Convert.ToDouble(Console.ReadLine());
+      
+      Console.WriteLine(num1 + num2);
 
       Console.ReadLine();
 
