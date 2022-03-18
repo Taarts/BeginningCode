504e9d86b95726d49869630d14ec1f0f6c1dea71
Tricia Amheiser <tricia@taarts.net>
2022-03-18 18:48:26 -0400

    First Array

--- Program.cs
+++ Program.cs
@@ -6,20 +6,15 @@ namespace BeginningCode
   {
     static void Main(string[] args)
     { 
-      string color, pluralNoun, celebrity;
-
-      Console.Write("Enter a color: ");
-      color = Console.ReadLine();
-
-      Console.Write("Enter a plural noun: ");
-      pluralNoun = Console.ReadLine();
-
-      Console.Write("Enter a celebrity: ");
-      celebrity = Console.ReadLine();
-
-      Console.WriteLine("Roses are " + color);
-      Console.WriteLine(pluralNoun + " are blue");
-      Console.WriteLine("I love " + celebrity);
+    
+    int [] luckyNumbers = { 5, 6, 18, 79, 125, 223};
+    string [] friends = new string [6];
+    friends[0] = "Mark";
+    friends[1] = "Lisa";
+    friends[2] = "Suzie";
+    friends[3] = "Susan";
+    friends[4] = "Nick";
+    Console.WriteLine( friends[4] );
       
       Console.ReadLine();
 
