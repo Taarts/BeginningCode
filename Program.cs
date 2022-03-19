2633980e565b6733f8650cdc0359d09476d13fd7
Tricia Amheiser <tricia@taarts.net>
2022-03-19 01:08:03 -0400

    While

--- Program.cs
+++ Program.cs
@@ -7,44 +7,18 @@ namespace BeginningCode
     static void Main(string[] args)
     { 
      
-      Console.WriteLine(GetDay(6));
+      int index = 1;
+      while (index <= 5) 
+      {
+        Console.WriteLine(index);
+        index++;
+      }
     
       Console.ReadLine();
 
-    }
+  
 
-    static string GetDay(int dayNum) {
-      string dayName;
-        switch (dayNum)
-        {
-          case 0:
-              dayName = "Sunday";
-              break;
-          case 1:
-              dayName = "Monday";
-              break;
-          case 2:
-              dayName = "Tuesday";
-              break;
-          case 3:
-              dayName = "Wednesday";
-              break;
-          case 4:
-              dayName = "Thursday";
-              break;
-          case 5:
-              dayName = "Friday";
-              break;
-          case 6:
-              dayName = "Saturday";
-              break;
-
-          default:
-            dayName = "Invalid Day Number";
-            break;
-        }
-
-      return dayName;
+    
     }
 
   }
