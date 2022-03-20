10fea555f5be3932c84ffa27d03fdc0ed9772494
Tricia Amheiser <tricia@taarts.net>
2022-03-20 16:33:11 -0400

    guees the word

--- Program.cs
+++ Program.cs
@@ -5,15 +5,35 @@ namespace BeginningCode
   class Program
   {
     static void Main(string[] args)
-    { 
-     
-      int index = 6;
-      do
+    {
+      string secretWord = "secretword";
+      string guess = "";
+      int guessCount = 0;
+      int guessLimit = 3;
+      bool outOfGuesses = false;
+
+      while(guess != secretWord && !outOfGuesses)
+
       {
-        Console.WriteLine(index);
-        index++;
-      }  while (index <= 5);
-    
+        if(guessCount < guessLimit)
+        {
+          Console.Write("Enter guess: ");
+          guess = Console.ReadLine();
+          guessCount++;
+        }
+        else
+        {
+          outOfGuesses = true;
+        }
+
+      }
+      if(outOfGuesses)
+      {
+      Console.Write("Oops, out of guesses");  
+      }
+      else
+      Console.Write("Congrats, you win!");
+
       Console.ReadLine();
 
   
