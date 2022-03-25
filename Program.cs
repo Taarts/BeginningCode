09d374e04eafbc88e258aee7d3c4d3858d00bc10
Tricia Amheiser <tricia@taarts.net>
2022-03-25 19:38:10 -0400

    Constructors

--- Program.cs
+++ Program.cs
@@ -6,19 +6,13 @@ namespace BeginningCode
   {
     static void Main(string[] args)
     {
-       Book book1 = new Book();
-       book1.title = "Harry Potter";
-       book1.author = "JK Rowling";
-       book1.pages = 452;
-      
-       Book book2 = new Book();
-       book2.title = "The Twin Towers";
-       book2.author = "JRR Tolkien";
-       book2.pages = 722;
+       Book book1 = new Book("Harry Potter", "Harry Potter", 452);
+       Book book2 = new Book("The Two Towers", "Tolkien", 722);
+       book2.title = "Return of the King";
 
 
-       Console.WriteLine(book2.pages);
-     
+     Console.WriteLine(book2.title);
+
      Console.ReadLine();
     }
 
