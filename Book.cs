db50239365a3bcf255d94a73b309935c146a46bf
Tricia Amheiser <tricia@taarts.net>
2022-03-25 19:22:54 -0400

    classes and objects intro

---
+++ Book.cs
@@ -0,0 +1,9 @@
+namespace BeginningCode
+{
+    public class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,23 +6,18 @@ namespace BeginningCode
   {
     static void Main(string[] args)
     {
-      try 
-      {
-      Console.Write("Enter a number: ");
-      int num1 = Convert.ToInt32(Console.ReadLine());
-      Console.Write("Enter another number: ");
-      int num2 = Convert.ToInt32(Console.ReadLine());
+       Book book1 = new Book();
+       book1.title = "Harry Potter";
+       book1.author = "JK Rowling";
+       book1.pages = 452;
+      
+       Book book2 = new Book();
+       book2.title = "The Twin Towers";
+       book2.author = "JRR Tolkien";
+       book2.pages = 722;
 
-      Console.WriteLine(num1 / num2);
-      }
-      catch(DivideByZeroException e)
-      {
-          Console.WriteLine(e.Message);
-      }
-      catch (FormatException e)
-      {
-        Console.WriteLine(e.Message);
-      }
+
+       Console.WriteLine(book2.pages);
      
      Console.ReadLine();
     }
